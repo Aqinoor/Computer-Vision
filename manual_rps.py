68a5006d5b5aae977515850b384129d26acf472c
Aqinoor <105247520+Aqinoor@users.noreply.github.com>
2022-09-02 19:46:02 +0100

    Add files via upload

--- manual_rps.py
+++ manual_rps.py
@@ -1,6 +1,4 @@
-from importlib.machinery import WindowsRegistryFinder
 import random
-
 
 class RPS:
     def __init__(self):
@@ -11,29 +9,55 @@ class RPS:
     def get_computer_choice(self):
         return random.choice(self.choices)
 
-    # randomly take an option from choices and assigned it to computer_choice
-    # Return computer_choice
-
     def get_user_choice(self):
-        user_choice = input("Enter Rock, Paper or Scissors")
-        return user_choice
+    
+        user_choice = input("Choose Rock, Paper or Scissors: ")
 
-    def get_winner(self, user_choice, computer_choice): # 0 = Draw, 1 = User winner, 2 Computer winner
+        while True:
+            if user_choice in choice:
+                RPS.get_winner(self, user_choice)
+                break
+            else:
+                print("Please enter either Rock, Paper or Scissors")
+            break  
+        pass
+        
+
+    def get_winner(self, user_choice, computer_choice): # 0 = Draw, 1 = User winner, -1 Computer winner
         if user_choice == computer_choice:
             return 0
         elif user_choice == 'Rock':
             if computer_choice == 'Scissors':
                 return 1
             else:
-                return 2
+                return -1
         elif user_choice == 'Paper':
             if computer_choice == 'Rock':
                 return 1
             else:
-                return 2
+                return -1
         elif user_choice == 'Scissors':
             if computer_choice == 'Paper':
                 return 1
             else:
-                return 2
+                return -1
+        pass
+            
+def play(choice):
+    game = RPS(choice)
+
+    game.get_computer_choice()
+    game.get_user_choice()
+    pass
+
+if __name__ == '__main__':
+    choice = ["Rock", "Paper", "Scissors"]
+    play(choice)
         
+
+
+
+
+
+
+
