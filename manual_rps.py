1679b9aac8514747721695c3a7841fe6b0c81580
Aqinoor <105247520+Aqinoor@users.noreply.github.com>
2022-09-05 21:18:50 +0100

    Add files via upload

--- manual_rps.py
+++ manual_rps.py
@@ -7,49 +7,45 @@ class RPS:
         self.choices = ['Rock', 'Paper', 'Scissors']
 
     def get_computer_choice(self):
-        return random.choice(self.choices)
+        computer_choice = random.choice(self.choices)
+        return computer_choice
 
     def get_user_choice(self):
     
         user_choice = input("Choose Rock, Paper or Scissors: ")
-
-        while True:
-            if user_choice in choice:
-                RPS.get_winner(self, user_choice)
-                break
-            else:
-                print("Please enter either Rock, Paper or Scissors")
-            break  
-        pass
+        return user_choice
         
 
     def get_winner(self, user_choice, computer_choice): # 0 = Draw, 1 = User winner, -1 Computer winner
         if user_choice == computer_choice:
-            return 0
-        elif user_choice == 'Rock':
-            if computer_choice == 'Scissors':
-                return 1
-            else:
-                return -1
-        elif user_choice == 'Paper':
-            if computer_choice == 'Rock':
-                return 1
-            else:
-                return -1
-        elif user_choice == 'Scissors':
-            if computer_choice == 'Paper':
-                return 1
-            else:
-                return -1
-        pass
+            print(f"'computer_choice': {computer_choice}, 'user_choice': {user_choice}") 
+            print("Draw!") 
+        elif (user_choice == 'Rock' and computer_choice == 'Scissors') or \
+             (user_choice == 'Paper' and computer_choice == 'Rock') or \
+             (user_choice == 'Scissors' and computer_choice == 'Paper'):
+            print(f"'computer_choice': {computer_choice}, 'user_choice': {user_choice}")
+            self.user_wins += 1
+            print("User Wins") 
+        else:
+            print(f"'computer_choice': {computer_choice}, 'user_choice': {user_choice}")
+            self.computer_wins += 1
+            print("Computer Wins")
             
 def play(choice):
-    game = RPS(choice)
-
-    game.get_computer_choice()
-    game.get_user_choice()
-    pass
-
+    rounds = 0
+    game = RPS()
+    while game.user_wins < 3 or game.computer_wins < 3:      
+        computer_choice = game.get_computer_choice() 
+        user_choice = game.get_user_choice()
+        game.get_winner(user_choice, computer_choice)
+        if game.user_wins == 3:
+            print("You won!")
+            exit()
+        elif game.computer_wins == 3:
+            print("You lose!")
+            exit()
+    
+    
 if __name__ == '__main__':
     choice = ["Rock", "Paper", "Scissors"]
     play(choice)
